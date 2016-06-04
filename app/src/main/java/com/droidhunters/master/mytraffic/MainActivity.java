dd839754213822ba8d39a1af173c09cf39866536
droidhunters <info.droidhunters@gmail.com>
2016-06-04 15:02:46 +0700

    Finish 4june

--- app/src/main/java/com/droidhunters/master/mytraffic/MainActivity.java
+++ app/src/main/java/com/droidhunters/master/mytraffic/MainActivity.java
@@ -1,14 +1,43 @@
 package com.droidhunters.master.mytraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit ประกาศตัวแปร หน่วยความจำของ memory
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Bind Widget
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+        //controlerButton
+        controllerButton();
+
     }//Method หรือ เมดธอดหลัก
+
+    private void controllerButton() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // sound effect การใส่เสียง
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.lion);
+                MediaPlayer.start();
+            } //ประกาศ new เพื่อดึง method มาใช้งาน
+        });
+
+    } //controllButton
 } //Main Class คลาสหลัก
 
